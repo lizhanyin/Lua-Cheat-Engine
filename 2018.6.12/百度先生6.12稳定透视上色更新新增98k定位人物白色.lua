99f977b73c3a551911618e7151594294c22dc701
j110 <1039227140@qq.com>
2018-06-12 21:08:49 +0800

    2018.6.12

---
+++ 2018.6.12/百度先生6.12稳定透视上色更新新增98k定位人物白色.lua
@@ -0,0 +1,233 @@
+﻿gg.toast("百度先生 ")
+mima='1039227140';
+
+v_value=gg.prompt({'百度先生QQ1039227140 ， '},{[1]=0},{[1]='number'});
+
+
+
+
+function m()
+--密码正确
+end
+
+
+function end_c()
+print("密码错误");
+os.exit();
+end
+
+
+
+if v_value[1] == mima then
+m()
+else
+end_c()
+end
+
+gg.alert('百度先生荣誉出品')
+
+gg.alert("转载注明版权,话不多说谢谢")
+
+
+gg.alert("如果你是买的,那么恭喜你被骗了")
+
+
+function Main()
+menu1 = gg.choice({
+' 1.联发科透视(最快,大厅开启)',
+' 2.高通骁龙通用透视(最稳,大厅开启)',
+' 3.麒麟透视(大厅开启)',
+' 4.835透视(大厅开启)',
+' 5.人物绿色(大厅开启)',
+' 6.人物白色(大厅开启)',
+' 7.范围打击(落地开启，无后范围只能开一个)',
+' 8.微无后坐力(落地开启)',
+' 9.上帝视角(落地开启)',
+' 10.98K近处定位(落地开启)',
+' 10.大厅开启透视上色,进入素质广场潜水憋气（解决落地无透视问题)',
+' 【退出脚本程序】'},
+nil,'百度先生6.12刺激战场辅助,[只发布上分功能]')
+if menu1 == 1 then a() end
+if menu1 == 2 then b() end
+if menu1 == 3 then c() end
+if menu1 == 4 then d() end
+if menu1 == 5 then e() end
+if menu1 == 6 then e1() end
+if menu1 == 7 then e2() end
+if menu1 == 8 then e3() end
+if menu1 == 9 then e4() end
+if menu1 == 10 then e5() end
+if menu1 == 11 then e6() end
+if menu1 == 11 then Exit() end
+XGCK=-1
+end
+-------------------------------------
+function a()
+   gg.clearResults()gg.searchNumber('10000;0.5', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)gg.searchNumber('0.5', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)gg.getResults(100)gg.editAll('0', gg.TYPE_FLOAT)gg.toast('联发科透视开启成功')
+end
+
+
+
+function b() 
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("4.814603e21;3.5032462e-44;3.7615819e-37;2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(30)
+gg.editAll("120", gg.TYPE_FLOAT)
+gg.toast("透视已开启25%")
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("-5.5693206e-40;4.814603e21;3.7615819e-37;2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(30)
+gg.editAll("120", gg.TYPE_FLOAT)
+gg.toast("透视已开启50%")
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("5.1848043e-44;-1.0285578e-38;3.7615819e-37;2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(30)
+gg.editAll("120", gg.TYPE_FLOAT)
+gg.toast("透视已开启75%")
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("304.00009155273;3.7615819e-37;2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(30)
+gg.editAll("120", gg.TYPE_FLOAT)
+gg.toast("高通骁龙透视开启成功")
+
+end
+
+
+function c()
+gg.setRanges(gg.REGION_BAD)  gg.searchNumber("2;-1;0;1;-127::", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)  gg.searchNumber("2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)gg.getResults(20)gg.editAll("20", gg.TYPE_FLOAT)gg.toast("Eric；透视已成功开启20%")gg.clearResults()gg.setRanges (gg.REGION_BAD)gg.searchNumber("4,141D;4.7408155e21;-5.5693206e-40;4.814603e21;3.7615819e-37;2:", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)gg.searchNumber("2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)gg.getResults(2)gg.editAll("100", gg.TYPE_FLOAT)gg.toast("Eric;透视已成功开启90%")gg.clearResults()gg.setRanges (gg.REGION_BAD)gg.searchNumber("-1.0285578e-38;3.7615819e-37;2;-1;1;-127::300", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)gg.searchNumber("2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)gg.getResults(20)gg.editAll("120", gg.TYPE_FLOAT)gg.toast("透视开启成功")
+end
+
+
+
+function d()
+gg.clearResults()gg.setRanges(gg.REGION_BAD)gg.searchNumber("4.814603e21;3.7615819e-37;2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)gg.searchNumber("2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)gg.getResults(30)gg.editAll("100", gg.TYPE_FLOAT)gg.toast("开启一半")gg.clearResults()gg.searchNumber("-1.0285578e-38;3.7615819e-37;2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)gg.searchNumber("2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)gg.getResults(30)gg.editAll("100", gg.TYPE_FLOAT)gg.toast("马上咯")gg.clearResults()gg.searchNumber("304.00009155273;3.7615819e-37;2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)gg.searchNumber("2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)gg.getResults(30)gg.editAll("100", gg.TYPE_FLOAT)gg.toast("透视开启成功")
+
+
+
+end
+
+function e()
+
+gg.clearResults()
+gg.setRanges (gg.REGION_BAD)
+gg.searchNumber('32,769;-2,134,900,722',gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber('32769', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(10)
+gg.editAll('32777', gg.TYPE_DWORD)
+  gg.toast("人物绿色开启成功")
+   
+
+end
+
+
+function e1()
+
+gg.clearResults()gg.setRanges(gg.REGION_BAD)gg.searchNumber("539,246,596;8200D;2.4903147e21F", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)gg.searchNumber("8200", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)gg.getResults(30)gg.editAll("8300", gg.TYPE_DWORD)gg.clearResults()gg.toast("人物白色开启成功")
+
+end
+
+
+function e2()
+
+gg.clearResults()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("88.15017700195;15:512", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("15", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(2)
+  gg.editAll("300", gg.TYPE_FLOAT)
+  gg.toast("加载中...")
+  gg.clearResults()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("-88.66608428955;26:512", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("26", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(2)
+  gg.editAll("300", gg.TYPE_FLOAT)
+  gg.toast("请稍候...")
+  gg.clearResults()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("90.4850692749;27.25;28:512", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("27.25;28", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(2)
+  gg.editAll("300", gg.TYPE_FLOAT)
+  gg.toast("范围打击开启成功")
+
+
+
+end
+
+
+function e3()
+
+
+
+
+
+gg.clearResults() gg.searchNumber("1868784978;1850305641;28518", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("1868784978", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(1000)
+gg.editAll("1868756421", gg.TYPE_DWORD)
+gg.clearResults() 
+gg.searchNumber("1750294898;1415932769;1819307365", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)  
+gg.searchNumber("1750294898;1415932769;1819307365", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(1000)
+gg.editAll("100000", gg.TYPE_DWORD)
+gg.toast("微后坐力开启成功")
+  
+
+
+end
+
+
+
+function e4()
+
+
+
+gg.clearResults() 
+gg.setRanges(gg.REGION_ANONYMOUS) gg.searchNumber("220;25;178", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1) 
+ gg.searchNumber("220", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1) 
+gg.getResults(100) 
+gg.editAll("500", gg.TYPE_FLOAT) 
+gg.toast("上帝视角开启成功")
+
+end
+
+
+
+function e5()
+gg.searchNumber("-1.4943189620972;-2.3106904029846;-1.5036518573761;0.33800649642944;0.19816112518311::340", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(100)
+  gg.editAll("9999", gg.TYPE_FLOAT)
+  gg.toast("98k定位开启成功")
+end
+
+function e5()
+gg.alert("你干嘛点开?你是不是傻。")
+
+
+end
+
+
+function Exit()
+  print('百度先生荣誉出品 ')
+  os.exit()
+end
+cs = "百度"
+while true do
+  if gg.isVisible(true) then
+    XGCK = 1
+    gg.setVisible(false)
+  end
+  gg.clearResults()
+  if XGCK == 1 then
+    Main()
+  end
+end
